a03cde56ff124ca882c6b69caa840fc63a59de31
rohittandale <rohittandale@LAPTOP-IPAN66N1>
2025-04-07 16:23:15 -0400

    Add project files.

---
+++ TravelAgent/Program.cs
@@ -0,0 +1,29 @@
+using Microsoft.OpenApi.Models;
+using Scalar.AspNetCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+//if (app.Environment.IsDevelopment())
+//{
+//    app.MapOpenApi();
+//    app.MapScalarApiReference(); // Maps the Scalar UI endpoint
+//}
+app.MapOpenApi();
+app.MapScalarApiReference(); // Maps the Scalar UI endpoint
+
+app.UseHttpsRedirection();
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
