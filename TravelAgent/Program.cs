9fd1aaf11c123112f24d5d09a987296f40782693
512.rt <rohit.developer90@gmail.com>
2025-04-09 10:35:38 -0400

    app insights configure

--- TravelAgent/Program.cs
+++ TravelAgent/Program.cs
@@ -14,6 +14,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllers();
 
+builder.Services.AddApplicationInsightsTelemetry();
+
 builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowAll", policy =>
